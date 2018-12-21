3d25b27173c17289f247b0e90abf71423e0487b6
tuyjoy92@yahoo.fr <tuyjoy92@yahoo.fr>
2018-12-21 08:22:49 +0200

    addind code in js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ var q4=document.forms["myform"]["question4"].value;
 var q5=document.forms["myform"]["question5"].value;
 var marks=0;
 if(q1=="js")
+{
+    marks=marks+20;
+    }
+    if(q2=="no")
+    {
+        marks=marks+20;
+    }
