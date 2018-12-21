f0a65c4585ddbe69725484c755bf62e1620ad00b
tuyjoy92@yahoo.fr <tuyjoy92@yahoo.fr>
2018-12-21 15:07:39 +0200

    readme

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-function mytest()
-{
-var q1=document.forms["myform"]["question1"].value;
-var q2=document.forms["myform"]["question2"].value;
-var q3=document.forms["myform"]["question3"].value;
-var q4=document.forms["myform"]["question4"].value;
-var q5=document.forms["myform"]["question5"].value;
-var marks=0;
-if(q1=="js")
-{
-    marks=marks+20;
-    }
-if(q2=="search()")
-{
-    marks=marks+20;
-    }
-if(q3=="Cookies")
-{
-    marks=marks+20;
-}
-if(q4=="Camelcase")
-{
-    marks=marks+20;   
-}
-if(q5=="Coding")
-{
-    marks=marks+20;   
-}
-alert("your score is:"+marks);
-}
 
+$(document).ready(function (){
+    $('#blanksform').submit(function(event){
+        var q1=document.forms["myform"]["question1"].value;
+        var q2=document.forms["myform"]["question2"].value;
+        var q3=document.forms["myform"]["question3"].value;
+        var q4=document.forms["myform"]["question4"].value;
+        var q5=document.forms["myform"]["question5"].value;
+        var marks=0;
+        if(q1=="js")
+        {
+           marks=marks+20;
+           }
+        if(q2=="search()")
+        {
+           marks=marks+20;
+           }
+        if(q3=="Cookies")
+        {
+           marks=marks+20;
+        }
+        if(q4=="Camelcase")
+        {
+           marks=marks+20;
+        }
+        if(q5=="Coding")
+        {
+           marks=marks+20;
+        }
+        $(".output").show();
+        $("#display").text("Your score is: " + result);
+  
+ event.preventDefault();
+        
+       
+ })
+ });
+ 
+ 
 
